4e8b3ac8fcdb1176040bdbbbe5e853c94e2203bb
Rajeevveera24 <rajeevamazon1@yahoo.in>
2020-10-13 16:43:23 +0530

    passed privilege as context in all views

--- orders/views.py
+++ orders/views.py
@@ -1,17 +1,23 @@
 from django.http import HttpResponse
 from django.shortcuts import render, redirect, get_object_or_404
-from .models import Order, Shop
+from .models import Order, Shop, User_Type
 from django.views import View
 from django.contrib.auth.mixins import LoginRequiredMixin as LRM
 from django.urls import reverse_lazy, reverse
 from .forms import CreateShopForm, CreateOrderForm
 
 def home(response):
-	return render(response, "orders/home.html", {})
+    priv = False
+    if response.user.is_authenticated:
+        priv = get_object_or_404(User_Type, user = response.user).privilege
+    return render(response, "orders/home.html", {'privilege': priv})
 
 def view(response):
     if response.user.is_authenticated:
-        return render(response, "orders/view.html", {})
+        user_type = get_object_or_404(User_Type, user = response.user)
+        ctx = dict()
+        ctx['privilege' ] = user_type.privilege
+        return render(response, "orders/view.html", ctx)
     else:
         return redirect("/login")
 
@@ -19,19 +25,19 @@ class ShopUpdateView(LRM, View):
     template_name = 'orders/shopupdate_form.html'
     success_url = reverse_lazy('home')
 
-    def get(self, request):
-        shop = get_object_or_404(Shop, user = self.request.user)
+    def get(self, response):
+        shop = get_object_or_404(Shop, user = self.response.user)
         form = CreateShopForm(instance = shop)
         ctx = {'form': form}
-        return render(request, self.template_name, ctx)
+        return render(response, self.template_name, ctx)
 
-    def post(self, request):
-        shop = get_object_or_404(Shop, user = self.request.user)
-        form = CreateShopForm(request.POST, instance = shop)
+    def post(self, response):
+        shop = get_object_or_404(Shop, user = self.response.user)
+        form = CreateShopForm(response.POST, instance = shop)
 
         if not form.is_valid():
             ctx = {'form': form}
-            return render(request, self.template_name, ctx)
+            return render(response, self.template_name, ctx)
         
         shop.save()
 
@@ -41,20 +47,20 @@ class ShopUpdateView(LRM, View):
 #     template_name = 'orders/ordercreate_form.html'
 #     success_url = reverse_lazy('view')
 
-#     def get(self, request):
+#     def get(self, response):
 #         form = CreateOrderForm()
 #         ctx = {'form': form}
-#         return render(request, self.template_name, ctx)
+#         return render(response, self.template_name, ctx)
 
-#     def post(self, request):
-#         form = CreateOrderForm(request.POST)
+#     def post(self, response):
+#         form = CreateOrderForm(response.POST)
 
 #         if not form.is_valid():
 #             ctx = {'form': form}
-#             return render(request, self.template_name, ctx)
+#             return render(response, self.template_name, ctx)
 
 #         order = form.save(commit = False)
-#         order.user = request.user
+#         order.user = response.user
 #         order.save(commit = True)
 
 #         return redirect(self.success_url)
@@ -63,19 +69,22 @@ class OrderUpdateView(LRM, View):
     template_name = 'orders/orderupdate_form.html'
     success_url = reverse_lazy('view')
 
-    def get(self, request):
-        order = get_object_or_404(Order, user = self.request.user)
+    def get(self, response):
+        order = get_object_or_404(Order, user = response.user)
         form = CreateOrderForm(instance = order)
         ctx = {'form': form}
-        return render(request, self.template_name, ctx)
+        user_type = get_object_or_404(User_Type, user = response.user)
+        # ctx = dict()
+        ctx['privilege'] = user_type.privilege
+        return render(response, self.template_name, ctx)
 
-    def post(self, request):
-        order = get_object_or_404(Order, user = self.request.user)
-        form = CreateOrderForm(request.POST, instance = order)
+    def post(self, response):
+        order = get_object_or_404(Order, user = response.user)
+        form = CreateOrderForm(response.POST, instance = order)
 
         if not form.is_valid():
             ctx = {'form': form}
-            return render(request, self.template_name, ctx)
+            return render(response, self.template_name, ctx)
         
         order.save()
 
