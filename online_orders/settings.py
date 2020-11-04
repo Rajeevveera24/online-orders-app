5ed60fa31345a3d1407c7ff3573a15b49100c53b
Rajeevveera24 <rajeevamazon1@yahoo.in>
2020-11-04 19:23:47 +0530

    changes to db - switch to postgres

--- online_orders/settings.py
+++ online_orders/settings.py
@@ -78,10 +78,21 @@ WSGI_APPLICATION = 'online_orders.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR + '/' + 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR + '/' + 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'arun_db',
+        'USER': 'arun_user',
+        'PASSWORD': 'arun#1234',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
@@ -140,3 +151,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+
+import dj_database_url
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
