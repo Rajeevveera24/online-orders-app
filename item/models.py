8aea569d84842dfd4f433c060de1123f6e3d0c91
Rajeevveera24 <rajeevamazon1@yahoo.in>
2021-04-03 18:36:56 +0530

    Added price field to items

--- item/models.py
+++ item/models.py
@@ -4,6 +4,7 @@ class Item(models.Model):
 	name = models.CharField(max_length = 50, unique = True)
 	desc = models.CharField(max_length = 100)
 	unit = models.CharField(max_length = 10, default = "No.s")
+	price = models.FloatField(null=True)
 
 	def __str__(self):
 		return self.name
